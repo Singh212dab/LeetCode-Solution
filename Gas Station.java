a2c1231e139e8388b23ed61bfff00deab53fb0af
Dabblu <71687992+Singh212dab@users.noreply.github.com>
2022-05-23 11:01:02 +0530

    Add files via upload

---
+++ Gas Station.java
@@ -0,0 +1,22 @@
+class Solution {
+public int canCompleteCircuit(int[] gas, int[] cost) {
+ int sumRemaining = 0;
+int total = 0; 
+int start = 0;
+for (int i = 0; i < gas.length; i++) {
+int remaining = gas[i] - cost[i];
+if (sumRemaining >= 0) {
+sumRemaining += remaining;
+} else {
+sumRemaining = remaining;
+start = i;
+}
+total += remaining;
+}
+if (total >= 0){
+return start;
+}else{
+return -1;
+}
+}   
+}
